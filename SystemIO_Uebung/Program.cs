58d5184f92b5d762d92b375673e2f2129349cbf7
Mert <58mertaydin58@gmail.com>
2024-09-19 12:22:12 +0200

    Projektdateien hinzufügen.

---
+++ SystemIO_Uebung/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemIO_Uebung
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string tempDirectory = @"C:\Mert";
+            string subdir = tempDirectory + @"\NET";
+            string subdir1 = tempDirectory + @"\SUBNET";
+
+            Console.WriteLine($"Current directory: {Directory.GetCurrentDirectory()}");
+
+            if (!Directory.Exists(tempDirectory))
+            {
+                Directory.CreateDirectory(tempDirectory);
+                Console.WriteLine($"Directory {tempDirectory} was created");
+            }
+            if (!Directory.Exists(subdir))
+            {
+                Directory.CreateDirectory(subdir);
+                Console.WriteLine($"Directory {subdir} was created");
+            }
+            if (!Directory.Exists(subdir1))
+            {
+                Directory.CreateDirectory(subdir1);
+                Console.WriteLine($"Directory {subdir1} was created");
+            }
+
+            Directory.Delete(subdir);
+
+
+            //Directory.Move(subdir, subdir1 + @"\Lukas");
+            Console.ReadLine();
+        }
+    }
+}
